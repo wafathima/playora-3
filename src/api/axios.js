26a5048bf90d8b4bfdbeeaf7e676a9ad40e22456
wafathima <wafathima15@gmail.com>
2026-01-06 12:55:21 +0530

    playora-fine

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://playora-backend.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -16,7 +16,7 @@ API.interceptors.request.use((config) => {
 });
 
 export const adminAPI = axios.create({
-  baseURL: "https://playora-backend.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
