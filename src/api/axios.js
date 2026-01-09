f4972f576a52eb18cb4774b2d8e3db9f4fcb28ea
wafathima <wafathima15@gmail.com>
2026-01-09 20:22:34 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://playora-backend.onrender.com/",
+  baseURL: "https://playora-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
@@ -16,7 +16,7 @@ API.interceptors.request.use((config) => {
 });
 
 export const adminAPI = axios.create({
-  baseURL: "https://playora-backend.onrender.com/",
+  baseURL: "https://playora-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
